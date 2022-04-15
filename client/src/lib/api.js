b5b65414a41b256f4f21d6776a751ea0e30d8909
hackerrank <me@hackerrank.com>
2022-04-15 12:19:48 +0300

    first commit

---
+++ client/src/lib/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BaseURL = "http://localhost:5500"
+
+export async function addNewEvent(eventData) { 
+    const {photoName, photographer, exhibition, country, year, style, photo} = eventData
+    const response = await axios.post(`${BaseURL}`, {photoName, photographer, exhibition, country, year, style, photo});
+    return response;
+}
+
+export async function getAllPhotosByExhibition(exhibition) { 
+    const response = await axios.get(`${BaseURL}/${exhibition}`);
+    return response;
+}
+
+export async function getAllPhotosByCountry(country) { 
+    const response = await axios.get(`${BaseURL}/${country}`);
+    return response;
+}
